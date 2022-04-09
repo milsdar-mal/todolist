54270d71ac7782371e92086e77cb881e6d329715
milsdar-mal <lyahovss@mer.ci.nsu.ru>
2022-04-09 11:50:01 +0700

    js какието что то

---
+++ aboba.js
@@ -0,0 +1,6 @@
+document.getElementById("add").onclick = function () {
+    //alert("aboba");
+    let spisok = document.querySelector("ul.spisok")
+    let element = document.createElement("li")
+
+}
